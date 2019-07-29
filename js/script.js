fa94d30657bf26118461eb0c240e3b4664b445f3
Liberty88 <39170066+Liberty88@users.noreply.github.com>
2019-07-29 17:44:16 +0200

    1
    1

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 let slide;
 function slideCreator(){
     slide = $('.page-banner').slick({
-        nextArrow: '<button type="button" class="slick-next"> <img src="../img/next.svg" class="page-banner__arrow-icon page-banner__arrow_next" alt="next" title="next"> </button>',
-        prevArrow: '<button type="button" class="slick-prev"> <img src="../img/prev.svg" class="page-banner__arrow-icon page-banner__arrow_prev" alt="prev" title="prev"> </button>',
+        nextArrow: '<button type="button" class="slick-next"> <img src="img/next.svg" class="page-banner__arrow-icon page-banner__arrow_next" alt="next" title="next"> </button>',
+        prevArrow: '<button type="button" class="slick-prev"> <img src="img/prev.svg" class="page-banner__arrow-icon page-banner__arrow_prev" alt="prev" title="prev"> </button>',
     });
 }
 
